54136feee7ca5da5626cc074b0e916a37327032b
DanielEAndriani <danielandriani@gmail.com>
2022-09-14 01:01:30 -0300

    prueba ok API whatsapp con valor fijo de numero destino

--- routes/whatsapp.routes.js
+++ routes/whatsapp.routes.js
@@ -3,22 +3,36 @@ const api = express.Router();
 const whatsappController = require('../controllers/whatsapp.controller');
 
 
-api.post('https://graph.facebook.com/v13.0/106030762244022/messageshttps://graph.facebook.com/v13.0/106030762244022/messages', function (req,res){
+api.post('https://graph.facebook.com/v13.0/106030762244022/messages', async function (req,res){
     console.log("prueba");
-    res.json (
+    
+    try{
+        const usuario = await axios.get(
+            'https://graph.facebook.com/v13.0/106030762244022/messages',
+            {
+            headers:{
+                     'Authorization': token
+                    }
+            },
+        
+            res.json (
        
-{ 
-    "messaging_product": "whatsapp",
- "to": "543815446283",
-  "type": "template",
-   "template": 
-   { "name": "hello_world",
-    "language":
-     { "code": "en_US" }
-   } 
-}
-    );
-
+                { 
+                    "messaging_product": "whatsapp",
+                 "to": "543815446283",
+                  "type": "template",
+                   "template": 
+                   { "name": "hello_world",
+                    "language":
+                     { "code": "en_US" }
+                   } 
+                }
+                    )           
+            )
+        
+   }catch(error){
+        console.log(error)
+   }
 });
 
 module.exports = api; 
