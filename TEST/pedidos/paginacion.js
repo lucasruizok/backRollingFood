f9cdba9d4e5a87c68644a67dcd1c679cb2b55a35
DanielEAndriani <danielandriani@gmail.com>
2022-09-22 18:19:41 -0300

    envio whatsapp desde boton, falta verificar seleccion de templates

--- TEST/pedidos/paginacion.js
+++ TEST/pedidos/paginacion.js
@@ -1,7 +1,7 @@
 /*------------------------------
 Archivo que contiene todos los pedidos en la BD, se puede ver mas y borrar a c/u
 ------------------------------*/
-
+const token= "EAAZAUWhDG8ZAUBAJcbnZBgzs8wWGAV6xuV4kNcmJliIFAG8VZC1f7kgLFCcN9d2sPaQckCHfZCe6AJnP2P8QzUNXAjfB6xLllRz06ZCwPC77PMI80mQ6sEdl8Iw4x5sZB1P3lERUcDlkGdBGIHa0WsM7ftELLEzOqD3vT8u9KgU8u831Boi5SXYeIBEKgJIZATspGMIhXMLA2QZDZD"
 
 
 const URL =`http://localhost:3400`
@@ -31,7 +31,7 @@ searchInput.addEventListener(`keyup`, async (event)=> {
 function listPedidos(pedidos){
     listaHTML.innerHTML= ``;                                         //para blanquer pantalla
      pedidos.map ( (elemento) => {
-        console.log(elemento)
+        //console.log(elemento)
         listaHTML.innerHTML= listaHTML.innerHTML + `<br>` + elemento.usuario.firstName +elemento.usuario.lastName +
         `  <button onclick="getSpecificUser(event)" id=${elemento._id} > ver mas </button>` +
         ` <button onclick="updateSpecificUser(event)" id=${elemento._id}> update </button>`+ 
@@ -78,12 +78,53 @@ function getSpecificUser(elemento){
 
 */
 
+function whatsapp (phone){
+    
+    let phone1= "\"" + phone +"\""
+    console.log("se envia ", phone1)
+
+    var data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "to": phone1,
+        "type": "template",
+        "template": {
+        "name": "hello_world",
+        "language": {
+            "code": "en_US"
+        }
+        }
+    });
+    
+    var config = {
+        method: 'post',
+        url: 'https://graph.facebook.com/v13.0/106030762244022/messages',
+        headers: { 
+        'Authorization': 'Bearer EAAZAUWhDG8ZAUBAOZC1LXBFRWRVsUY2qZBrI2Kf8WjGeZBaYEBBjEEVx4z39rL3FUyqcuDxheQ0LVWGZBS7U2xqU4clZCErJKzZAgtODkcJP0uS8Q00XQi2RUtYckb5JILV38FSYoZAPObIdZC4PBrKpPenA1y1JRrz8Dj29ZBSZA979JJJmTEplDXug', 
+        'Content-Type': 'application/json'
+        },
+        data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+}
+
+
+
 
 //Obtener campo phone del usuario desde el pedido a traves de su id
  async function Pedidos(elemento){
    
      const pedido = await axios.get(`${URL}/pedidos/${elemento.target.id}`)
+     let phone=pedido.data.pedidos.usuario.phone;
      console.log(pedido.data.pedidos.usuario.phone)
+     whatsapp(phone)
  }
 
  
--- TEST/usuarios/whatsapp.js
+++ TEST/usuarios/whatsapp.js
@@ -1,52 +1,17 @@
 const token= "EAAZAUWhDG8ZAUBAJcbnZBgzs8wWGAV6xuV4kNcmJliIFAG8VZC1f7kgLFCcN9d2sPaQckCHfZCe6AJnP2P8QzUNXAjfB6xLllRz06ZCwPC77PMI80mQ6sEdl8Iw4x5sZB1P3lERUcDlkGdBGIHa0WsM7ftELLEzOqD3vT8u9KgU8u831Boi5SXYeIBEKgJIZATspGMIhXMLA2QZDZD"
-/*let data
+const token2= "EAAZAUWhDG8ZAUBAOZC1LXBFRWRVsUY2qZBrI2Kf8WjGeZBaYEBBjEEVx4z39rL3FUyqcuDxheQ0LVWGZBS7U2xqU4clZCErJKzZAgtODkcJP0uS8Q00XQi2RUtYckb5JILV38FSYoZAPObIdZC4PBrKpPenA1y1JRrz8Dj29ZBSZA979JJJmTEplDXug"
 
 
-data=  
-    {
-    "messaging_product": "whatsapp",
-    "to": "543815446283",
-    "type": "template",
-    "template": 
-        { 
-        "name": "hello_world",
-        "language": { "code": "en_US" }
-        } 
-    }
+
+function whatsapp (phone){
+   
+    const celular= "54" + phone                 //completo numero con la estructura para el envio
+    console.log("numero completo", celular)
 
 
-console.log(data)
-
-async function whatsapp(){
-console.log("ingreso")
-try{
-
-/*
-     const usuario = await axios({
-        url: "https://graph.facebook.com/v13.0/106030762244022/messages",
-        method: "POST",
-        header: {
-          "Content-Type": "application/json",
-          'Authorization':  ' EAAZAUWhDG8ZAUBAOZC1LXBFRWRVsUY2qZBrI2Kf8WjGeZBaYEBBjEEVx4z39rL3FUyqcuDxheQ0LVWGZBS7U2xqU4clZCErJKzZAgtODkcJP0uS8Q00XQi2RUtYckb5JILV38FSYoZAPObIdZC4PBrKpPenA1y1JRrz8Dj29ZBSZA979JJJmTEplDXug' 
-        },
-        data: data
-      });   
-
-}catch(error){
-    console.log(error)
-}
-
-
-}
-*/
-let celular= "543815446283"
-
-
-function whatsapp (){
-
-    var data = JSON.stringify({
+    let objeto= {
         "messaging_product": "whatsapp",
-        "to": celular,
+        "to": "numeroDestino",
         "type": "template",
         "template": {
         "name": "hello_world",
@@ -54,7 +19,11 @@ function whatsapp (){
             "code": "en_US"
         }
         }
-    });
+    }
+    
+    objeto.to = celular
+   
+    var data = JSON.stringify(objeto);
     
     var config = {
         method: 'post',
@@ -75,3 +44,5 @@ function whatsapp (){
     });
 
 }
+
+
