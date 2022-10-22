ba1a15163fbdebaf6a9c317a700c46bf60d2a3c9
lucasruizok <lucaruiz734@gmail.com>
2022-10-22 20:54:54 -0300

    subir a heroku

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const password = require('./config/config').dbPassword;
 const url = `mongodb+srv://lucasRuiz734:${password}@cluster0.thtdlho.mongodb.net/?retryWrites=true&w=majority`;
 const mongoose = require('mongoose');
 const express = require('express');
-const port = 3400;
-
+const port = process.env.port || 3400;
 const app = require('./app');
 // CONEXION A BASE DE DATOS ROLLING FOOD
 
 async function rollingFoodConnect(){
     try{
-        console.log('Iniciando BD')
         await mongoose.connect(url)
-        console.log('***CONEXION EXITOSA**')
         app.listen(port, () =>{
             console.log(`CONEXION A BD EXITOSA CON EXPRESS EN EL PUERTO ${port}`)
         })
