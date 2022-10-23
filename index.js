8f1011d5a1d2be4fbca928fb1f8436d882aabf9b
lucasruizok <lucaruiz734@gmail.com>
2022-10-23 12:09:02 -0300

    creacion variables de entorno locales

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
-const password = require('./config/config').dbPassword;
-const url = process.env.MONGODB_URI || `mongodb+srv://lucasRuiz734:${password}@cluster0.thtdlho.mongodb.net/?retryWrites=true&w=majority`;
+require('dotenv').config({path: 'variables.env'});
+const url = process.env.MONGODB_URL;
 const mongoose = require('mongoose');
 const express = require('express');
-const port = process.env.port || 3400;
+const port = process.env.PORT || 3400;
+const host = process.env.HOST || '0.0.0.0';
 const app = require('./app');
-require('dotenv').config();
 // CONEXION A BASE DE DATOS ROLLING FOOD
 
 async function rollingFoodConnect(){
     try{
         await mongoose.connect(url)
-        app.listen(port, () =>{
+        app.listen(port,host, () =>{
             console.log(`CONEXION A BD EXITOSA CON EXPRESS EN EL PUERTO ${port}`)
         })
         }catch(error){
